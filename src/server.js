66e620be964c9162ffab770952bbf77a59608e89
Alexandre Froes <133459638+AleFroes@users.noreply.github.com>
2024-04-08 15:16:52 -0300

    Update server.js
    Alteração do arquivo para comunicação com o dB.json

--- src/server.js
+++ src/server.js
@@ -1,12 +1,57 @@
 const http = require('http');
+const fs = require('fs');
 
-const hostname = '0.0.0.0'; // Pode ser '0.0.0.0' para torná-lo acessível externamente
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Olá, mundo! \n');
+  if (req.method === 'POST' && req.url === '/enviar-dados') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      const newData = JSON.parse(body);
+      
+      fs.readFile('db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          res.statusCode = 500;
+          res.end('Erro ao ler dados do arquivo JSON');
+          return;
+        }
+
+        let jsonData = JSON.parse(data);
+        jsonData = { ...jsonData, ...newData };
+
+        fs.writeFile('database/db.json', JSON.stringify(jsonData), (err) => {
+          if (err) {
+            console.error(err);
+            res.statusCode = 500;
+            res.end('Erro ao gravar dados no arquivo JSON');
+            return;
+          }
+          res.statusCode = 200;
+          res.end('Dados gravados com sucesso');
+        });
+      });
+    });
+  } else if (req.method === 'GET' && req.url === '/obter-dados') {
+    fs.readFile('database/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.end('Erro ao ler dados do arquivo JSON');
+        return;
+      }
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(data);
+    });
+  } else {
+    res.statusCode = 404;
+    res.end('Rota não encontrada');
+  }
 });
 
 server.listen(port, hostname, () => {
