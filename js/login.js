fd94ff814ccd5e4af602ca3e1e21ab1755782a2e
Alexandre Froes <133459638+AleFroes@users.noreply.github.com>
2024-04-21 20:16:22 -0300

    Update login.js
    Alteração na forma de acesso, agora pelo email.

--- js/login.js
+++ js/login.js
@@ -1,17 +1,16 @@
       function validateForm() {
-      var username = document.getElementById("username").value;
+      var username = document.getElementById("email").value;
       var password = document.getElementById("password").value;
 
       // Aqui você pode adicionar lógica para validar o nome de usuário e senha.
       // Por exemplo, você pode compará-los com valores armazenados em um banco de dados.
 
-      if (username === "client" && password === "client") {
+      if (username === "client@client.com" && password === "client") {
         alert("Login bem-sucedido!");
-        window.location.href = "order.html"; // Redireciona para admin.html
+        window.location.href = "order.html"; // Redireciona para order.html
         return false; // Impede o envio do formulário padrão
       } else {
         alert("Nome de usuário ou senha incorretos.");
         return false;
       }
     }
-
